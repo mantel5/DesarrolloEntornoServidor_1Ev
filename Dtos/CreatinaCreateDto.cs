ee12daec4b95a9ebe9369b90dd781d54c55acc88
mantel5 <a28310@svalero.com>
2025-11-27 19:34:40 +0100

    comentarios básicos

--- Dtos/CreatinaCreateDto.cs
+++ Dtos/CreatinaCreateDto.cs
@@ -2,21 +2,21 @@ namespace SuplementosAPI.Dtos
 {
     public class CreatinaCreateDto
     {
-        // --- Nivel 1 ---
+        // Nivel 1
         public string Nombre { get; set; }
         public decimal Precio { get; set; }
         public int Stock { get; set; }
         public string Descripcion { get; set; }
         public string Imagen { get; set; }
 
-        // --- Nivel 2 ---
+        // Nivel 2
         public string Categoria { get; set; }
         public double PesoKg { get; set; }
 
-        // --- Nivel 3: Creatina ---
+        // Nivel 3
         public string Sabor { get; set; }
-        public string Tipo { get; set; }        // Monohidrato...
-        public string Formato { get; set; }     // Polvo...
+        public string Tipo { get; set; }       
+        public string Formato { get; set; }    
         public bool SelloCreapure { get; set; }
         public bool EsMicronizada { get; set; }
         public int DosisDiariaGr { get; set; }
