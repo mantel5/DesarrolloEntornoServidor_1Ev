114e5633fc83131f38fcfc3152160d6656e21a91
mantel5 <a28310@svalero.com>
2025-11-27 19:36:44 +0100

    comentarios básicos

--- Dtos/ProteinaCreateDto.cs
+++ Dtos/ProteinaCreateDto.cs
@@ -2,21 +2,21 @@ namespace SuplementosAPI.Dtos
 {
     public class ProteinaCreateDto
     {
-        // --- Nivel 1: ProductoBase ---
+        // Nivel 1
         public string Nombre { get; set; }
         public decimal Precio { get; set; }
         public int Stock { get; set; }
         public string Descripcion { get; set; }
         public string Imagen { get; set; }
 
-        // --- Nivel 2: SuplementoBase ---
+        // Nivel 2
         public string Categoria { get; set; }
         public double PesoKg { get; set; }
 
-        // --- Nivel 3: Proteina ---
+        // Nivel 3
         public string Sabor { get; set; }
-        public string Tipo { get; set; }        // Whey, Isolate...
-        public int Porcentaje { get; set; }     // Pureza
+        public string Tipo { get; set; }        
+        public int Porcentaje { get; set; }     
         public bool EsSinLactosa { get; set; }
     }
 }
