13320f233ee38a56b7e78b3e92b5c86f41b0fe3b
mantel5 <a28310@svalero.com>
2025-11-27 19:32:53 +0100

    comentarios básicos

--- Dtos/BebidaCreateDto.cs
+++ Dtos/BebidaCreateDto.cs
@@ -2,20 +2,20 @@ namespace SuplementosAPI.Dtos
 {
     public class BebidaCreateDto
     {
-        // --- Nivel 1 ---
+        // Nivel 1 
         public string Nombre { get; set; }
         public decimal Precio { get; set; }
         public int Stock { get; set; }
         public string Descripcion { get; set; }
         public string Imagen { get; set; }
 
-        // --- Nivel 2 (Macros) ---
+        // Nivel 2 
         public double Calorias { get; set; }
         public double Proteinas { get; set; }
         public double Carbohidratos { get; set; }
         public double Grasas { get; set; }
 
-        // --- Nivel 3: Bebida ---
+        // Nivel 3
         public string Sabor { get; set; }
         public int Mililitros { get; set; }
         public bool TieneGluten { get; set; }
