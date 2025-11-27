0b7da6118b8c841cb58ba00996b1747fe4621b3a
mantel5 <a28310@svalero.com>
2025-11-27 19:38:07 +0100

    comentarios básicos

--- Dtos/SalsaCreateDto.cs
+++ Dtos/SalsaCreateDto.cs
@@ -2,20 +2,20 @@ namespace SuplementosAPI.Dtos
 {
     public class SalsaCreateDto
     {
-        // --- Nivel 1: ProductoBase ---
+        // Nivel 1
         public string Nombre { get; set; }
         public decimal Precio { get; set; }
         public int Stock { get; set; }
         public string Descripcion { get; set; }
         public string Imagen { get; set; }
 
-        // --- Nivel 2: ComidaBase (Macros) ---
+        // Nivel 2
         public double Calorias { get; set; }
         public double Proteinas { get; set; }
         public double Carbohidratos { get; set; }
         public double Grasas { get; set; }
 
-        // --- Nivel 3: Salsa ---
+        // Nivel 3
         public string Sabor { get; set; }
         public bool EsPicante { get; set; }
         public bool EsZero { get; set; }
