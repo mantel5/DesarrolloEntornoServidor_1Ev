3d9a5ce0000d0d8406a96a4328817d375e7e9ae1
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-11-27 19:54:10 +0100

    eliminar comentarios innecesarios

--- Program.cs
+++ Program.cs
@@ -14,13 +14,11 @@ builder.Services.AddScoped<ITortitasRepository, TortitasRepository>();
 builder.Services.AddScoped<ITortitasService, TortitasService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
